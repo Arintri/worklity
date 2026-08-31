ce6450ed2282eeb3516f395b0b21aa56e29c7261
Arintri <backofficefkt@gmail.com>
2026-08-31 07:43:22 +0530

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,46 @@
+export default function sitemap() {
+  const baseUrl = "https://worklity-ten.vercel.app";
+
+  return [
+    {
+      url: baseUrl,
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/land-area-calculator`,
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/percentage-calculator`,
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/age-calculator`,
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/about`,
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: `${baseUrl}/privacy-policy`,
+      changeFrequency: "yearly",
+      priority: 0.4,
+    },
+    {
+      url: `${baseUrl}/disclaimer`,
+      changeFrequency: "yearly",
+      priority: 0.4,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+  ];
+}
